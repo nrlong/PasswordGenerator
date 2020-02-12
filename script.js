26e82bf99ceebd7b972ea89adf8f5a07f0403144
Nathan Long <nrlong13@gmail.com>
2020-02-11 19:48:52 -0500

    completed JS script for generator.  updated css and html to reflect needed accessability changes

--- script.js
+++ script.js
@@ -1,8 +1,49 @@
+let generated = document.getElementById("generatedPass");
+let numberCount = document.getElementById("char-count-box");
+let upperCase = document.getElementById("uppercase");
+let lowerCase = document.getElementById("lowercase");
+let numbers = document.getElementById("numbers");
+let symbols = document.getElementById("symbol");
+let submit = document.getElementById("submit-button");
+
+
 let randomFunction = {
     lower: getRandomLower,
     upper: getRandomUpper,
     number: getRandomNumber,
     symbol: getRandomSymbol
+}
+
+submit.addEventListener("click", () => {
+    let hasUpper = upperCase.checked;
+    let hasLower = lowerCase.checked;
+    let hasNumber = numbers.checked;
+    let hasSymbol = symbols.checked;
+    let numberTotal= +numberCount.value;
+
+    generated.innerText = generatePassword(hasUpper, hasLower, hasNumber, hasSymbol, numberTotal);
+
+});
+
+    function generatePassword( lower, upper, number, symbol, numberTotal){
+        let generatedPassword = " ";
+        let typesCount = lower + upper + number + symbol;
+        let typesArray = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+    
+    if(typesCount === 0) {
+        return " ";
+    }
+
+    for(let i = 0; i < numberTotal; i += typesCount){
+        typesArray.forEach(type => {
+            let funcName = Object.keys(type)[0];
+            generatedPassword += randomFunction[funcName]();
+        });
+    }
+
+    let finalPassword = generatedPassword.slice(0, numberTotal);
+
+    return finalPassword;
 }
 
 //functions for generating characters needed for the generator
